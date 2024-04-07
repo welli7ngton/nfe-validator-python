15144cfa2f33aa3f31dff9896d7d488753489650
welli7ngton <wellingtonasilva45@gmail.com>
2024-04-06 21:10:14 -0300

    refactor: turning all methods in classmethods to increase tests cases

--- app/validate.py
+++ app/validate.py
@@ -7,7 +7,7 @@ file = Path(__file__).resolve()
 parent, root = file.parent, file.parents[1]
 sys.path.append(str(root))
 
-from my_exceptions.exceptions import InvalidNFEKeyLenght
+from my_exceptions.exceptions import InvalidNFEKeyLength
 
 
 class GenerateNfeDV:  
@@ -19,28 +19,28 @@ class GenerateNfeDV:
         if '.' in nfe_number:
             nfe_number = cls.remove_dot(nfe_number)
 
-        if cls.veryfi_nfe_len(nfe_number):
+        if cls.verify_nfe_len(nfe_number):
             nfe_number_int = cls.generate_list_of_int(nfe_number)
             numbers_to_mulitplication = cls.reverse_list(cls.generate_numbers_to_multiplication(nfe_number_int))
-            sum_of_list_items = cls.get_sum_of_lists_items(nfe_number_int, numbers_to_mulitplication)
+            sum_of_list_items = cls.get_sum_of_lists_multiplications(nfe_number_int, numbers_to_mulitplication)
             return cls.get_DV(sum_of_list_items)
         else:
-            raise InvalidNFEKeyLenght(len(nfe_number))
+            raise InvalidNFEKeyLength(len(nfe_number))
 
-    @staticmethod
-    def veryfi_nfe_len(nfe_number: str) -> bool:
+    @classmethod
+    def verify_nfe_len(cls, nfe_number: str) -> bool:
         return len(nfe_number) == 43
 
-    @staticmethod
-    def generate_list_of_int(_list: List[str]) -> List[int]:
+    @classmethod
+    def generate_list_of_int(cls, _list: List[str]) -> List[int]:
         return [int(n) for n in _list]
 
-    @staticmethod
-    def reverse_list(_list: List[int]) -> List[int]:
+    @classmethod
+    def reverse_list(cls, _list: List[int]) -> List[int]:
         return _list[len(_list)::-1]
 
-    @staticmethod
-    def generate_numbers_to_multiplication(nfe_numbers: List[int]) -> List[int]:
+    @classmethod
+    def generate_numbers_to_multiplication(cls, nfe_numbers: List[int]) -> List[int]:
         counter = 2
         numbers_multiplication = []
         for i in range(len(nfe_numbers), 0, -1):
@@ -51,19 +51,20 @@ class GenerateNfeDV:
             counter += 1
         return numbers_multiplication
 
-    @staticmethod
-    def get_sum_of_lists_items(_list1: List[int], _list2: List[int]) -> int:
-        return sum([_list1[i] * _list2[i] for i in range(43)])
+    @classmethod
+    def get_sum_of_lists_multiplications(cls, _list1: List[int], _list2: List[int]) -> int:
+        return sum([_list1[i] * _list2[i] for i in range(len(_list1))])
 
-    @staticmethod
-    def get_DV(sum_of_lists: int) -> int:
+    @classmethod
+    def get_DV(cls, sum_of_lists: int) -> int:
+        print(sum_of_lists)
         DV = 11 - (sum_of_lists % 11)
         return DV if DV <= 9 else 0
     
-    @staticmethod
-    def remove_white_space(nfe_number: str) -> str:
+    @classmethod
+    def remove_white_space(cls, nfe_number: str) -> str:
         return "".join([char for char in nfe_number if char != ' '])
     
-    @staticmethod
-    def remove_dot(nfe_number: str) -> str:
+    @classmethod
+    def remove_dot(cls, nfe_number: str) -> str:
         return "".join([char for char in nfe_number if char != '.'])   
